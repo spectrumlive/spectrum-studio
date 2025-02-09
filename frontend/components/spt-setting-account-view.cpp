0e03c6b2c3a45c2edb95f1cda735507d3b06eb0f
nanusorn <nanusorn@photpipat.com>
2025-02-10 01:27:14 +0700

    User's account view in Setting->General tab

--- frontend/components/spt-setting-account-view.cpp
+++ frontend/components/spt-setting-account-view.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Scrooge McDuck on 7/2/2025 AD.
+// Created by Nanusorn Photpipat on 7/2/2025 AD.
 //
 
 #include "spt-setting-account-view.hpp"
@@ -19,27 +19,7 @@ SPTSettingAccountView::~SPTSettingAccountView()
 
 void SPTSettingAccountView::initUi()
 {
-
 	QString filePath = ":/settings/images/settings/profile.svg";
-//	if (PLSLoginUserInfo::getInstance()->getAuthType() == LOGIN_USERINFO_EMAIL) {
-//		ui->email->setText(PLSLoginUserInfo::getInstance()->getEmail());
-//	} else {
-//		QString snsIconPath = pls_prism_user_thumbnail_path();
-//		if (QFileInfo::exists(snsIconPath)) {
-//			filePath = snsIconPath;
-//		}
-//		ui->email->setVisible(false);
-//		ui->pushButton_change_pwd->setVisible(false);
-//		ui->horizontalLayout_2->addStretch(1);
-//
-//		if ("whale" == PLSLoginUserInfo::getInstance()->getAuthType().toLower()) {
-//			ui->userIconLabel->setPlatformPixmap(QString(":/resource/images/sns-profile/img-%1-profile.png").arg(PLSLoginUserInfo::getInstance()->getAuthType().toLower()),
-//							     QSize(34 * 4, 34 * 4));
-//		} else {
-//			ui->userIconLabel->setPlatformPixmap(QString(":/resource/images/sns-profile/img-%1-profile.svg").arg(PLSLoginUserInfo::getInstance()->getAuthType().toLower()),
-//							     QSize(34 * 4, 34 * 4));
-//		}
-//	}
 	ui->userIconLabel->setPixmap(filePath, QSize(110, 110));
 }
 
